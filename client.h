488365d5ed11ad1c5b704ca373f2eca56c676572
AakashBasnet <aakashbasnet@AakashBasnets-MacBook-Pro.local>
2018-03-07 12:48:14 -0500

    Initial commit of TCP protocols

---
+++ client.h
@@ -0,0 +1,14 @@
+//
+//  client.h
+//  TCP_PROGRAMMING
+//
+//  Created by AakashBasnet on 3/6/18.
+//
+
+#ifndef client_h
+#define client_h
+
+#include <stdio.h>
+
+
+#endif /* client_h */
---
+++ server.h
@@ -0,0 +1,13 @@
+//
+//  server.h
+//  TCP_PROGRAMMING
+//
+//  Created by AakashBasnet on 3/6/18.
+//
+
+#ifndef server_h
+#define server_h
+
+#include <stdio.h>
+
+#endif /* server_h */
